dca2c73ab114971a2a613e3288465b0b36355150
Hiroki Kumamoto <kumabook@live.jp>
2017-04-30 14:12:29 +0900

    Add error.rs

---
+++ src/error.rs
@@ -0,0 +1,73 @@
+use std;
+use std::str::FromStr;
+use std::fmt::{Display, Formatter, Result as FmtResult};
+use iron::prelude::*;
+use iron::IronError;
+use iron::status::Status;
+use iron::mime::Mime;
+use hyper;
+use urlencoded;
+use serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Error {
+    BadRequest,
+    Unprocessable,
+    NotFound,
+    Unexpected,
+}
+
+impl Error {
+    pub fn status(&self) -> Status {
+        match *self {
+            Error::BadRequest        => Status::BadRequest,
+            Error::Unprocessable     => Status::UnprocessableEntity,
+            Error::NotFound          => Status::NotFound,
+            Error::Unexpected        => Status::InternalServerError,
+        }
+    }
+
+    pub fn as_response(&self) -> Response {
+        Response::with((self.status(),
+                        Mime::from_str("application/json").ok().unwrap(),
+                        serde_json::to_string(&self).unwrap()))
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match *self {
+            Error::BadRequest            => write!(f, "BadRequest"),
+            Error::Unprocessable         => write!(f, "Unproccesable"),
+            Error::NotFound              => write!(f, "NotFound"),
+            Error::Unexpected            => write!(f, "UnexpectedError"),
+        }
+    }
+}
+
+impl From<Error> for IronError {
+    fn from(err: Error) -> IronError {
+        match err {
+            Error::BadRequest        => IronError::new(err, Status::BadRequest),
+            Error::Unprocessable     => IronError::new(err, Status::BadRequest),
+            Error::NotFound          => IronError::new(err, Status::NotFound),
+            Error::Unexpected        => IronError::new(err, Status::BadRequest),
+        }
+    }
+}
+
+impl From<urlencoded::UrlDecodingError> for Error {
+    fn from(_: urlencoded::UrlDecodingError) -> Error {
+        Error::BadRequest
+    }
+}
+
+impl From<hyper::Error> for Error {
+    fn from(_: hyper::Error) -> Error {
+        Error::BadRequest
+    }
+}
+
+impl std::error::Error for Error {
+    fn description(&self) -> &str { "" }
+}
